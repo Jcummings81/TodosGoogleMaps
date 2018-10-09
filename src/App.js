0da5b2df9478345cfc1d211b35bc7f825890474a
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-08 22:29:43 -0600

    geolocation working next to get markers working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css'
 
 
+
 class App extends Component {
 
   createMarker = (place) => {
@@ -13,12 +14,48 @@ class App extends Component {
 }
 
   callback = (results, status) => {
-    if (status == window.google.maps.places.PlacesServiceStatus.OK) {
+    if (status === window.google.maps.places.PlacesServiceStatus.OK) {
       for (let i = 0; i < results.length; i++) {
         let places = results[i];
         this.createMarker(places);
       }
     }
+  }
+
+
+  createMarker = (place) => {
+    let placeLoc = place.geometry.location;
+    let image = 'img/flag.png';
+    let marker = new window.google.maps.Marker({
+        map: this.map,
+        position: place.geometry.location,
+        title: place.name,
+        animation: window.google.maps.Animation.DROP,
+        icon: image
+    });
+
+    window.google.maps.event.addListener(marker, 'click', () => {
+        this.infowindow.setContent(place.name);
+        this.infowindow.open(this.map,marker);
+    });
+}
+
+
+  handleNoGeolocation = (errorFlag) => {
+    if (errorFlag) {
+      let content = 'Error: The Geolocation service failed.';
+    } else {
+      let content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    let options = {
+      map: this.map,
+      position: new window.google.maps.LatLng(40.7610, -111.8829),
+      content: this.content
+    };
+  
+    let infowindowLocation = new window.google.maps.InfoWindow(options);
+    this.map.setCenter(options.position);
   }
 
 componentDidMount() {
@@ -27,34 +64,63 @@ componentDidMount() {
   
 
   renderMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyC3GTxiL1yACghG2d7q_h2Jagj2MfajXdw&libraries=places&callback=initMap")
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyC3GTxiL1yACghG2d7q_h2Jagj2MfajXdw&libraries=places,geometry&callback=initMap")
     window.initMap = this.initMap
   }
 
+
   initMap = () => {
-    let DevPoint = new window.google.maps.LatLng(40.7610,-111.8829);
+
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+          let pos = new window.google.maps.LatLng(position.coords.latitude,
+                                     position.coords.longitude);
+
+          let infowindowLocation = new window.google.maps.InfoWindow({
+              map: map,
+              position: pos,
+              content: 'Found You!'
+          });
+
+          let request = {
+              location: pos,
+              radius: 3218.69,
+              types: ['dentist']
+          };
+
+          let infowindow = new window.google.maps.InfoWindow();
+          let service = new window.google.maps.places.PlacesService(map);
+          service.nearbySearch(request, this.callback);
+
+          map.setCenter(pos);
+
+      }, () => {
+          this.handleNoGeolocation(true);
+      });
+  } else {
+      // Browser doesn't support Geolocation
+      this.handleNoGeolocation(false);
+  }
+
+
+ 
+
+////////////////////////////////////////////////////////////////
+  let DevPoint = new window.google.maps.LatLng(40.7610,-111.8829);
 
    const map = new window.google.maps.Map(document.getElementById('map'), {
         center: DevPoint,
         zoom: 15
       });
   
-    let request = {
-      location: DevPoint,
-      radius: '500',
-      type: ['restaurant']
-    };
+  //   const request = {
+  //     location: DevPoint,
+  //     radius: '5000',
+  //     type: ['airport']
+  //   };
 
-    const service = new window.google.maps.places.PlacesService(map);
-    service.nearbySearch(request, this.callback);
-
-
-///////////////////////////////////////////////////
-
-    // const map = new window.google.maps.Map(document.getElementById('map'), {
-    //   center: {lat: -34.397, lng: 150.644},
-    //   zoom: 8,
-    // });
+  //   const service = new window.google.maps.places.PlacesService(map);
+  //   service.nearbySearch(request, this.callback);
 
 
   }
