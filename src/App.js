5fa8f1bc871182c433078d886fd870f2af8ee557
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-08 19:52:03 -0600

    due to lack of control of map api functionality, created a custom loader to be able to hopefully add much more

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
-import Gmap from './Gmap'
-import MapForm from './MapForm'
-
+import './App.css'
 
 
 class App extends Component {
+componentDidMount() {
+  this.renderMap()
+}
+  
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyC3GTxiL1yACghG2d7q_h2Jagj2MfajXdw&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8,
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-       <MapForm />
-       
-       <Gmap />
-     
-      </div>
+      <main>
+        <div id="map"/>
+      </main>
     );
   }
 }
+
+/*
+<script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC3GTxiL1yACghG2d7q_h2Jagj2MfajXdw&callback=initMap">
+    </script>
+*/
+
+function loadScript(url) {
+  let index = window.document.getElementsByTagName("script")[0]
+  let script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+
+//<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC3GTxiL1yACghG2d7q_h2Jagj2MfajXdw&callback=initMap" async defer> </script>
 
 export default App
