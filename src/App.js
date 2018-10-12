e2c5d40a7d2a80735349863b3b673c395fa7c220
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-12 07:11:40 -0600

    working autocomplete searchbox with icon setter!!!!

--- src/App.js
+++ src/App.js
@@ -120,6 +120,55 @@ componentDidUpdate(prevProps, prevState) {
           map.addListener('bounds_changed', function() {
             searchBox.setBounds(map.getBounds());
           });
+
+          var markers = [];
+          // Listen for the event fired when the user selects a prediction and retrieve
+          // more details for that place.
+          searchBox.addListener('places_changed', function() {
+            var places = searchBox.getPlaces();
+  
+            if (places.length == 0) {
+              return;
+            }
+  
+            // Clear out the old markers.
+            markers.forEach(function(marker) {
+              marker.setMap(null);
+            });
+            markers = [];
+  
+            // For each place, get the icon, name and location.
+            var bounds = new window.google.maps.LatLngBounds();
+            places.forEach(function(place) {
+              if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+              }
+              var icon = {
+                url: place.icon,
+                size: new window.google.maps.Size(71, 71),
+                origin: new window.google.maps.Point(0, 0),
+                anchor: new window.google.maps.Point(17, 34),
+                scaledSize: new window.google.maps.Size(25, 25)
+              };
+  
+              // Create a marker for each place.
+            markers.push(new window.google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
         
 
         map.setCenter(pos);
