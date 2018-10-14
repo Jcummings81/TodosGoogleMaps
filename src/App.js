084bc79b9dc3a6da2348bccf277afba601e7abbe
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-14 08:05:45 -0600

    markers now clickable

--- src/App.js
+++ src/App.js
@@ -1,95 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css'
 
 
 class App extends Component {
 
-  state = {
-    searches: '',
-    fire: false,
-    markers: [],
-    mp: null,
-    service: null,
-    infowindow: null,
-    x: '',
-    cleared: false,
-    searchBox: null,
-    input: 'hello'
-  }
 
 renderMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyB42fDSaBjwjE5-TIUwEy-WJIk3camVa2A&libraries=places,geometry&callback=initMap")
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyD9kLr4MK7Tu6wBgQRUDHh-_dW1oPEpQ9k&libraries=places,geometry&callback=initMap")
     window.initMap = this.initMap
   }
 
 componentDidMount() {
   this.renderMap()
-}
-
-componentDidUpdate(prevProps, prevState) {
-  if (prevState !== this.state) {
-    if (this.state.fire === true) {
-    this.renderMap()
-    this.setState({fire: false})
-    }
-  }
-}
-  
-
-  createMarker = (place) => {
-    const {mp, infowindow, service, } = this.state
-    let marker = new window.google.maps.Marker({
-        map: mp,
-        position: place.geometry.location,
-        title: place.name,
-        draggable: true
-        //animation: window.google.maps.Animation.DROP,
-
-    });
-
-    window.google.maps.event.addListener(marker, 'click', () => {
-        infowindow.setContent(place.name, this.mp);
-        infowindow.open(this.map, marker);
-       
-    });
-        return marker;
   }
 
-
-  callback = (results, status) => {
-      const {mp, service, infowindow, markers, cleared} = this.state
-    if ((status === window.google.maps.places.PlacesServiceStatus.OK) && !cleared) {
-
-      for (let i = 0; i < results.length; i++) {
-        let places = results[i];
-        let x =  this.createMarker(places, mp)
-        markers.push(x)
-        x.setMap(mp)
-        console.log(places.name)
-        
-      }
-    }
-  }
-
-  initMap = () => {
+initMap = () => {
     let DevPoint = new window.google.maps.LatLng(40.7610, -111.8829);
     const map = new window.google.maps.Map(document.getElementById('map'), {
         center: DevPoint,
         zoom: 15
       });
 
-    
-
     if(navigator.geolocation) {
           navigator.geolocation.getCurrentPosition((position) => {
           let pos = new window.google.maps.LatLng(position.coords.latitude,
                                      position.coords.longitude);
 
-          let infowindowLocation = new window.google.maps.InfoWindow({
+          let infowindow = new window.google.maps.InfoWindow({
               map: map,
               position: pos,
               content: 'Your Location!'
-          });
+                  });
 
 
           let marker = new window.google.maps.Marker({
@@ -97,29 +38,16 @@ componentDidUpdate(prevProps, prevState) {
             map: map,
             title: 'Hello World!',
             draggable: true
-          });
-
-          let request = {
-              location: pos,
-              radius: 50000,
-              types: [this.state.searches]
-          };
-
-
-
-          this.setState({infowindow: new window.google.maps.InfoWindow()})
-
-          this.setState({ service: new window.google.maps.places.PlacesService(map)})
-          this.state.service.nearbySearch(request, this.callback);
+                  });
 
           var input = document.getElementById('pac-input');
           var searchBox = new window.google.maps.places.SearchBox(input);
-          map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(input);
+          map.controls[window.google.maps.ControlPosition.BOTTOM_LEFT].push(input);
   
           // Bias the SearchBox results towards current map's viewport.
           map.addListener('bounds_changed', function() {
             searchBox.setBounds(map.getBounds());
-          });
+                  });
 
           var markers = [];
           // Listen for the event fired when the user selects a prediction and retrieve
@@ -130,131 +58,93 @@ componentDidUpdate(prevProps, prevState) {
             if (places.length == 0) {
               return;
             }
-  
-            // Clear out the old markers.
-            markers.forEach(function(marker) {
-              marker.setMap(null);
-            });
-            markers = [];
-  
-            // For each place, get the icon, name and location.
-            var bounds = new window.google.maps.LatLngBounds();
-            places.forEach(function(place) {
-              if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
+            
+                  // Clear out the old markers.
+                  markers.forEach( (marker) => {
+                  marker.setMap(null);
+                        });
+                            markers = [];
+                                      
+                  // For each place, get the icon, name and location.
+                  var bounds = new window.google.maps.LatLngBounds();
+                  places.forEach( (place, i) => {
+                    if (!place.geometry) {
+                      console.log("Returned place contains no geometry");
+                      return;
+                            }
+
+                    var icon = {
+                      url: place.icon,
+                      size: new window.google.maps.Size(71, 71),
+                      origin: new window.google.maps.Point(0, 0),
+                      anchor: new window.google.maps.Point(17, 34),
+                      scaledSize: new window.google.maps.Size(25, 25)
+                            };
+        
+                  // Create a marker for each place.
+                      markers.push(new window.google.maps.Marker({
+                        map: map,
+                        icon: icon,
+                        title: place.name,
+                        position: place.geometry.location
+                              }));
+
+                  //Add infowindow to each marker
+                      window.google.maps.event.addListener(markers[i], 'click', () => {
+                        infowindow.setContent(place.name, map);
+                        infowindow.open(map, markers[i]);
+                      })
+                
+                        if (place.geometry.viewport) {
+                          // Only geocodes have viewport.
+                          bounds.union(place.geometry.viewport);
+                          } else {
+                          bounds.extend(place.geometry.location);
+                                  }
+                                                            });
+                            map.fitBounds(bounds);
+                                    });
+        
+            map.setCenter(pos);
+
+              }, () => {
+                      this.handleNoGeolocation(true);
+                    }); 
+              } else {
+                  // Browser doesn't support Geolocation
+                  this.handleNoGeolocation(false);
               }
-              var icon = {
-                url: place.icon,
-                size: new window.google.maps.Size(71, 71),
-                origin: new window.google.maps.Point(0, 0),
-                anchor: new window.google.maps.Point(17, 34),
-                scaledSize: new window.google.maps.Size(25, 25)
-              };
-  
-              // Create a marker for each place.
-            markers.push(new window.google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });
-        
-
-        map.setCenter(pos);
-
-    
-        this.setState({mp: map})
-        
-      }, () => {
-          this.handleNoGeolocation(true);
-      });
-  } else {
-      // Browser doesn't support Geolocation
-      this.handleNoGeolocation(false);
-  }
-
   }
 
   handleNoGeolocation = (errorFlag) => {
-    if (errorFlag) {
-      let content = 'Error: The Geolocation service failed.';
-    } else {
-      let content = 'Error: Your browser doesn\'t support geolocation.';
-    }
+          if (errorFlag) {
+            let content = 'Error: The Geolocation service failed.';
+          } else {
+            let content = 'Error: Your browser doesn\'t support geolocation.';
+          }
 
-    let options = {
-      map: this.map,
-      position: new window.google.maps.LatLng(40.7610, -111.8829),
-      content: this.content
-    };
-
-
+          let options = {
+            map: this.map,
+            position: new window.google.maps.LatLng(40.7610, -111.8829),
+            content: this.content
+          };
   
+   
+
     let infowindowLocation = new window.google.maps.InfoWindow(options);
     this.map.setCenter(options.position);
-
-    
-
      
   }
 
- 
-
-handleChange = (e) => {
-    this.setState({ searches: e.target.value })
-
-}
-
-handleClick = (e) => {
-  this.setState({ fire: true })
-
-}
-
-
-clearResults = () => {
-  const {mp, markers} = this.state  
-  for (let i=0; i<markers.length; i++) {
-    let x = markers[i]
-    this.setState({x: null})
-    x.setMap(mp)
-  }
-  this.setState({cleared: true})
-}
-  
   render() {
     return (
-      <>
+      <Fragment>
         <div id="map"/>
-        <input onChange={this.handleChange} />
-        <button onClick={this.handleClick} onChange={this.searchBox}>
-          Search
-        </button>
-        <button onClick={this.searchBox}>
-          SearchBox
-        </button>
-        <button onClick={this.clearResults}>
-          Clear Map
-        </button>
-        <input id="pac-input" class="controls" type="text" placeholder="Search Box"></input>
-      </>
+        <input id="pac-input" class="controls" type="text" placeholder="Search Box" autofocus style={{height: "35px", fontSize: "12px"}}></input>
+      </Fragment>
     );
   }
 }
-
-
-
-
 
 function loadScript(url) {
   let index = window.document.getElementsByTagName("script")[0]
