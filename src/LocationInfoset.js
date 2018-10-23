58f7eae13b45a0f1fe55f6d483cc6e111f6c5a68
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-22 18:27:36 -0600

    full code for adding user labeled markers

--- src/LocationInfoset.js
+++ src/LocationInfoset.js
@@ -1,13 +1,20 @@
 import React, { Component, Fragment } from 'react';
 import './App.css'
-import {Form} from 'semantic-ui-react'
+
+var map = null;
+var markers = [];
+var counter = 0;
+var addMarker = null
+var deleteMarker = null
+var setMarkerTitle = null
+
 
 class Location extends Component {
 
   state = {lists: [] }
   
 renderMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBZ0MpvjEqSwWodG4vyHC63aNjJ3Cl8dBQ&libraries=places,geometry&callback=initMap")
+    loadScript("https://maps.googleapis.com/maps/api/js?key=&libraries=places,geometry&callback=initMap")
     window.initMap = this.initMap
   }
 
@@ -15,74 +22,128 @@ renderMap = () => {
     this.renderMap()
   }
   
+
 initMap = () => {
 
-  let DevPoint = new window.google.maps.LatLng(40.7610, -111.8829);
-  const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: DevPoint,
-      zoom: 15
+
+    let DevPoint = new window.google.maps.LatLng(40.7610, -111.8829);
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+        center: DevPoint,
+        zoom: 15
+      });
+      let infowindow = new window.google.maps.InfoWindow();
+
+      window.google.maps.event.addListener(map, 'click', function (event) {
+
+        addMarker(event.latLng);
     });
+
+    window.google.maps.event.addListener(infowindow, 'domready', function () {
+
+        var button, markerId, inputValue;
+
+        // Switch scenarii depending on infowindow contents
+        if (document.getElementById('deleteButton')) {
+
+            // Bind action for delete button
+            button = document.getElementById('deleteButton');
+            button.focus();
+            markerId = parseInt(button.getAttribute('data-id'));
+            button.onclick = function () {
+                
+                // Call deleteMarker function
+                deleteMarker(markerId);
+            };
+
+        } else {
+
+            // Bind action for set title button
+            button = document.getElementById('inputButton');
+            markerId = parseInt(button.getAttribute('data-id'));
+            button.onclick = function () {
+                
+                // Get input value and call setMarkerTitle function
+                inputValue = document.getElementById('nameinput').value;
+                setMarkerTitle(markerId, inputValue);
+            };
+            
+            document.getElementById('nameinput').focus();
+        }
+    });
+
   
-  var infowindow = new window.google.maps.InfoWindow();
-  var inwindow = new window.google.maps.InfoWindow();
-  var markers = [];
-  var counter = 0;
-  var marker
 
 
-  window.google.maps.event.addListener(map, 'click', function (event) {
+  //////////////////////////////////////////
 
-    addMarker(event.latLng);
-    });
-
-window.google.maps.event.addListener(inwindow, 'domready', function () {
-    var button = document.getElementById('inputButton');
-    var input = document.getElementById('nameinput').value;
-    button.onsubmit = function() {
-    marker.title = input;
-    inwindow.close();
-    };
-    });
-window.google.maps.event.addListener(infowindow, 'domready', function () {
-    var button = document.getElementById('deleteButton');
-    var id = parseInt(button.getAttribute('data-id'));  
-    button.onclick = function() {
-    deleteMarker(id);
-    };
-    });
-
-function addMarker(location) {
-  var marker = new window.google.maps.Marker({
-    position: location,
-    map: map,
-    id: counter
-    });
-
-    counter++;
-
- var inputForm = 'Name:  <input type="text" id="nameinput" size="31" maxlength="31" tabindex="1"/>' + '<input type="button" id="inputButton" value="Submit">';
-
-inwindow.setContent(inputForm);
-inwindow.open(map, marker);
-
-markers.push(marker);
-
-var deleteButton = '<button id="deleteButton" data-id="' + counter + '">Delete</button>';
-
-window.google.maps.event.addListener(marker, 'rightclick', function () {
-    infowindow.setContent(deleteButton);
-    infowindow.open(map, marker);
-    });
+// Function to set a marker title
+setMarkerTitle = (markerId, title) => {
+  markers[markerId].setTitle(title);
+  infowindow.close();
 }
 
-function deleteMarker(markerId) {
-    for (var i=0; i<markers.length; i++) {
-    if (markers[i].id === markerId) {
-        markers[i].setMap(null);
-    }
-    }
-}  
-  }
+// Function to add a marker
+addMarker = (location) => {
+
+  var inputForm;
+  counter++;
+
+  // Create marker
+  var marker = new window.google.maps.Marker({
+      position: location,
+      map: map,
+      id: counter
+  });
+
+  // Create title field and submit button
+  inputForm = 'Name:  <input type="text" id="nameinput" size="31" maxlength="31" value=""/>' + '<button id="inputButton" data-id="' + counter + '">Submit</button>';
+
+  // Set infowindow content
+  infowindow.setContent(inputForm);
+  infowindow.open(map, marker);
+
+  // Add marker to markers array
+  markers[counter] = marker;
+
+  var deleteButton = '<button id="deleteButton" data-id="' + counter + '">Delete</button>';
+
+  // Right click event (to present delete marker button)
+  window.google.maps.event.addListener(marker, 'rightclick', function () {
+      infowindow.setContent(deleteButton);
+      infowindow.open(map, marker);
+  });
+
+  // Left click event (to present fields to set title)
+  window.google.maps.event.addListener(marker, 'click', function () {
+
+      var markerTitle;
+      
+      // Check if marker has title to prevent adding "undefined" to input field
+      if (this.getTitle()) {
+          
+          markerTitle = this.getTitle();
+          
+      } else {
+          
+          markerTitle = '';
+      }
+
+      // Create title field and submit button
+      inputForm = 'Name:  <input type="text" id="nameinput" size="31" maxlength="31" tabindex="1" value="' + markerTitle + '"/>' + '<input type="button" id="inputButton" data-id="' + this.id + '" value="Submit">';
+
+      // Set infowindow content
+      infowindow.setContent(inputForm);
+      infowindow.open(map, marker);
+  });
+}
+
+// Function to delete a marker
+deleteMarker = (markerId) => {
+
+  markers[markerId].setMap(null);
+}
+}
+
 
   render() {
     return (
@@ -90,7 +151,7 @@ function deleteMarker(markerId) {
       <Fragment>
         <div id="map" hidden={this.props.hidemap}/>
       </Fragment>
-    )
+    );
   }
 }
 
